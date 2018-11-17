e52d84b807e812232a48e653cd5db9ebf39d5afc
wmompie <wmompie@gmail.com>
2018-11-17 00:46:46 -0500

    fixed and prevented memory leak from componentDidMount

--- src/components/Stopwatch.js
+++ src/components/Stopwatch.js
@@ -12,6 +12,10 @@ class Stopwatch extends Component {
     this.intervalID = setInterval(() => this.tick(), 100);
   }
 
+  componentWillUnmount() {
+    clearInterval(this.intervalID);
+  }
+
   tick = () => {
     if(this.state.isRunning) {
       const now = Date.now();
@@ -31,15 +35,21 @@ class Stopwatch extends Component {
     }
   }
 
+  handleReset = () => {
+    this.setState({elapsedTime: 0});
+  }
+
   render() {
+    const seconds = Math.floor(this.state.elapsedTime / 1000);
+
     return (
       <div className="stopwatch">
         <h2>Stopwatch</h2>
-        <span className="stopwatch-time">0</span>
+        <span className="stopwatch-time">{seconds}</span>
         <button onClick={this.handleStopwatch}>
           {this.state.isRunning ? 'Stop' : 'Start'}
         </button>
-        <button>Reset</button>
+        <button onClick={this.handleReset}>Reset</button>
       </div>
     );
   }
