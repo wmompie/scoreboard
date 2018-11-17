1d2b2018cd1244a4e000f1657ed4025a6b73325c
wmompie <wmompie@gmail.com>
2018-11-16 23:41:53 -0500

    built AddPlayerForm component

---
+++ src/components/AddPlayerForm.js
@@ -0,0 +1,32 @@
+import React, {Component} from 'react';
+
+class AddPlayerForm extends Component {
+
+  state = {
+    value: ""
+  }
+
+  handleValueChange = (e) => {
+    this.setState({value: e.target.value});
+  }
+
+  render() {
+    return (
+      <form>
+        <input 
+          type="text"
+          value={this.state.value}
+          onChange={this.handleValueChange}
+          placeholder="Enter a player's name"
+        />
+
+        <input 
+          type="submit"
+          value="Add Player"
+        />
+      </form>
+    );
+  }
+}
+
+export default AddPlayerForm;
