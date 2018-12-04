da2b8fe24612880a578ed65661d89c1abc8b6d05
wmompie <wmompie@gmail.com>
2018-12-04 00:06:13 -0500

    updated the initial player list and removed the crown icon

--- src/components/Context/index.js
+++ src/components/Context/index.js
@@ -1,87 +1,87 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 
 const ScoreboardContext = React.createContext();
 
 export class Provider extends Component {
-
   state = {
     players: [
       {
-        name: "Will",
+        name: 'Will',
         score: 0,
         id: 1
       },
       {
-        name: "Craig",
+        name: 'Craig',
         score: 0,
         id: 2
       },
       {
-        name: "PoorPocket",
+        name: 'Chris',
         score: 0,
         id: 3
       },
       {
-        name: "Nox",
+        name: 'Danny',
         score: 0,
         id: 4
       }
     ]
   };
 
-   // player id counter
-   prevPlayerId = 4;
+  // player id counter
+  prevPlayerId = 4;
 
-   handleScoreChange = (index, delta) => {
-     this.setState( prevState => ({
-       score: prevState.players[index].score += delta
-     }));
-   }
- 
-   handleAddPlayer = (name) => {
-     this.setState( prevState => {
-       return {
-         players: [
-           ...prevState.players,
-           {
-             name,
-             score: 0,
-             id: this.prevPlayerId += 1
-           }
-         ]
-       };
-     });
-   }
- 
-   handleRemovePlayer = (id) => {
-     this.setState( prevState => {
-       return {
-         players: prevState.players.filter(p => p.id !== id)
-       };
-     });
-   }
- 
-   getHighScore = () => {
-     const scores = this.state.players.map( p => p.score );
-     const highScore = Math.max(...scores);
-     if (highScore) {
-       return highScore;
-     } 
-     return null;
-   }
+  handleScoreChange = (index, delta) => {
+    this.setState(prevState => ({
+      score: (prevState.players[index].score += delta)
+    }));
+  };
 
+  handleAddPlayer = name => {
+    this.setState(prevState => {
+      return {
+        players: [
+          ...prevState.players,
+          {
+            name,
+            score: 0,
+            id: (this.prevPlayerId += 1)
+          }
+        ]
+      };
+    });
+  };
+
+  handleRemovePlayer = id => {
+    this.setState(prevState => {
+      return {
+        players: prevState.players.filter(p => p.id !== id)
+      };
+    });
+  };
+
+  getHighScore = () => {
+    const scores = this.state.players.map(p => p.score);
+    const highScore = Math.max(...scores);
+    if (highScore) {
+      return highScore;
+    }
+    return null;
+  };
 
   render() {
     return (
-      <ScoreboardContext.Provider value={{
-        players: this.state.players,
-        actions: {
-          changeScore: this.handleScoreChange,
-          removePlayer: this.handleRemovePlayer,
-          addPlayer: this.handleAddPlayer,
-          highScore: this.getHighScore
+      <ScoreboardContext.Provider
+        value={{
+          players: this.state.players,
+          actions: {
+            changeScore: this.handleScoreChange,
+            removePlayer: this.handleRemovePlayer,
+            addPlayer: this.handleAddPlayer,
+            highScore: this.getHighScore
           }
-        }}>
+        }}
+      >
         {this.props.children}
       </ScoreboardContext.Provider>
     );
--- src/components/Player.js
+++ src/components/Player.js
@@ -1,24 +1,23 @@
-import React, {PureComponent} from 'react';
+import React, { PureComponent } from 'react';
 import PropTypes from 'prop-types';
 import Counter from './Counter';
-import Icon from './Icon';
-import {Consumer} from './Context';
+import { Consumer } from './Context';
 
 class Player extends PureComponent {
-
   static propTypes = {
     index: PropTypes.number.isRequired
   };
 
   render() {
-    const {index} = this.props;
+    const { index } = this.props;
     return (
       <div className="player">
         <Consumer>
-          {({actions, players}) => (
+          {({ actions, players }) => (
             <span className="player-name">
-              <button className="remove-player" onClick={() => actions.removePlayer(players[index].id)}>✖</button>
-              <Icon />
+              <button className="remove-player" onClick={() => actions.removePlayer(players[index].id)}>
+                ✖
+              </button>
               {players[index].name}
             </span>
           )}
